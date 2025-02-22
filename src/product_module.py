9dfc8aeaaf9fd025db956f2c099ccb8245294644
sbike <mysilverbike@gmail.com>
2025-02-22 11:11:21 +0400

    task 4

--- src/product_module.py
+++ src/product_module.py
@@ -1,7 +1,7 @@
 class Product:
     name: str
     description: str
-    price: float
+    __price: float
     quantity: int
 
     def __init__(self, name, description, price, quantity):
@@ -10,34 +10,8 @@ class Product:
         """
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
-
-    # @classmethod
-    # def check_product_list(cls, name):
-    #     """
-    #     проверяет не встречалось ли name  в списке продуктов ранее
-    #     :param name:
-    #     :return: product or None
-    #     """
-    #     print("check_product_list in process")
-    #     for category in cls.categories:
-    #         for product in category.products:
-    #             if name == product.name:
-    #                 return product
-    #     return None
-
-    # @staticmethod
-    # def merge_products(price, quantity, product_doublet):
-    #     """
-    #     соединяет два продукта в один. Их количество складывается, цена выбирается максимальной.
-    #     :param price: Цена нового продукта.
-    #     :param quantity: Количество нового продукта.
-    #     :param product_doublet: Продукт из списка продуктов с одинаковым именем.
-    #     """
-    #     product_doublet.quantity += quantity
-    #     product_doublet.price = max(product_doublet.price, price)
-
 
     @classmethod
     def new_product(cls, product_dct):
@@ -47,6 +21,17 @@ class Product:
         :return: новый экземпляр класса продукт
         """
         return cls(*product_dct.values())
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        if new_price > 0:
+            self.__price = new_price
+        else:
+            print("Цена не должна быть нулевая или отрицательная")
 
 
 if __name__ == '__main__':
@@ -60,6 +45,20 @@ if __name__ == '__main__':
     dct = {"name": "Colnago", "description": "Best cycle for Giro", "price": 500_000.00, "quantity": 1}
     product2 = Product.new_product(dct)
     print("\n------name--description--price--quantity-----product2")
+    print(product2.name)
+    print(product2.description)
+    print(product2.price)
+    print(product2.quantity)
+
+    print('\n--------------new price----------')
+    product2.price = 777.77
+    print(product2.name)
+    print(product2.description)
+    print(product2.price)
+    print(product2.quantity)
+
+    print('\n--------------negative price------------')
+    product2.price = -777.77
     print(product2.name)
     print(product2.description)
     print(product2.price)
